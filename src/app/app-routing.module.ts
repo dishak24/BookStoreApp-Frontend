28bc27fccfc78ae09c2f9d8580be339f2278acb3
Disha <dishakamble24@gmail.com>
2025-05-14 23:52:36 +0530

    [Disha] Added: path for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuardService } from './services/auth-guard/auth-guard.service';
 
 const routes: Routes = 
 [
@@ -8,6 +10,11 @@ const routes: Routes =
     path: 'auth',
     component: AuthenticationComponent   
   }, 
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuardService],//property - true or false   
+  },
   {
     path: '',
     redirectTo: '/auth',
