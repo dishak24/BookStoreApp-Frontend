069c001f16f4f87fabacbc298ad949f8378b55ec
Disha <dishakamble24@gmail.com>
2025-05-14 14:10:46 +0530

    [Disha] Added: path for authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {
+    path: 'auth',
+    component: AuthenticationComponent   
+  }, 
+  {
+    path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
