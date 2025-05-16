dd96980136723008148a3502d23861325f89ef24
Disha <dishakamble24@gmail.com>
2025-05-17 04:19:49 +0530

    [Disha] added: implementation to add book in wishlist

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from 'src/app/services/book/book.service';
 
@@ -27,7 +28,11 @@ export class BookDetailsComponent implements OnInit
   bookId: number = 0;
   //book: any;
 
-  constructor(private route: ActivatedRoute, private bookService: BookService) {}
+  constructor(
+    private route: ActivatedRoute, 
+    private bookService: BookService,
+    private snackbar: MatSnackBar
+  ) {}
 
   @Input() book: any;
   @Output() back = new EventEmitter<void>();
@@ -54,5 +59,41 @@ export class BookDetailsComponent implements OnInit
       }
     });
   }
+
+  addToWishlist(bookId: number) 
+  {
+  const payload = { bookId };
+  console.log('Attempting to add book to wishlist:', payload);
+
+  this.bookService.addToWishlist(bookId).subscribe({
+    next: (result) => {
+      console.log('Book added to wishlist!', result);
+
+      this.snackbar.open('Book added to wishlist!', 'Close', {
+        duration: 3000,
+        panelClass: ['success-snackbar']
+      });
+    },
+    error: (err) => {
+      console.error('Adding to wishlist failed!!', err);
+
+      let errorMsg = 'Could not add book to wishlist. Please try again.';
+
+      if (err?.error?.message?.includes('Book is already in the wishlist')) {
+        errorMsg = 'Book already exists in your wishlist.';
+      } else if (err?.error?.message?.includes('Book not found')) {
+        errorMsg = 'Book not found.';
+      }
+
+      this.snackbar.open(errorMsg, 'Close', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      });
+    }
+  });
+}
+
+
+
 
 }
