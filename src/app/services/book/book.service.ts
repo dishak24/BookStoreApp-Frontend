2a3492b9efafb2f193a6d367b73698db66db5884
Disha <dishakamble24@gmail.com>
2025-05-19 02:51:26 +0530

    [Disha] Modified: APIs added

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -74,5 +74,46 @@ export class BookService
     console.log('Headers:', httpOption);
     return this.http.getApi('/api/wishlists', httpOption.headers);
   }
+
+  //remove book from wishlist
+  removeFromWishlist(bookId: number) 
+  {
+    let httpOption = {
+      headers: new HttpHeaders(
+      {
+        'Authorization': `Bearer ${this.token}`,
+        'Content-Type': 'application/json'
+      })
+    };
+    console.log('Headers:', httpOption);
+    return this.http.deleteApi(`/api/wishlists/${bookId}`, httpOption.headers);
+  }
+
+  //add book to cart
+  addToCart(bookId: number)
+  {
+    let httpOption = {
+      headers: new HttpHeaders(
+      {
+        'Authorization': `Bearer ${this.token}`,
+        'Content-Type': 'application/json'
+      })
+    };
+    console.log('Headers:', httpOption);
+    return this.http.postApi(`/api/carts?bookId=${bookId}`, {}, httpOption.headers);
+  }
+
+  // update book quantity in cart
+  updateCartQuantity(bookId: number, quantity: number) 
+  {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    });
+
+    const body = { quantity };
+    
+    return this.http.putApi(`/api/carts/${bookId}`, body, headers);
+  }
   
 }
