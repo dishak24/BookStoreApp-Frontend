c225e31a272d451b7aa27954c838abbf72709da6
Disha <dishakamble24@gmail.com>
2025-05-20 02:55:11 +0530

    [Disha] Modified: path as a child components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,9 @@ import { AuthGuardService } from './services/auth-guard/auth-guard.service';
 import { BooksComponent } from './components/books/books.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderPlaceComponent } from './components/order-place/order-place.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
 
 const routes: Routes = 
 [
@@ -17,20 +20,15 @@ const routes: Routes =
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [AuthGuardService],//property - true or false 
-    children: [
-      { path: '', component: BooksComponent }, ] 
-  },
-  {
-    path: 'books',
-    component: BooksComponent
-  },
-  {
-    path: 'books/:bookId',
-    component: BookDetailsComponent
-  },
-  {
-    path: 'wishlist',
-    component: WishlistComponent
+    children: 
+              [
+                { path: 'home', component: BooksComponent },
+                { path: 'books/:bookId',  component: BookDetailsComponent },
+                { path: 'wishlists', component: WishlistComponent },
+                { path: 'carts', component: CartComponent },
+                { path: 'purchase', component: OrderPlaceComponent },
+                { path: 'orders', component: OrderHistoryComponent},
+              ] 
   },
   {
     path: '',
