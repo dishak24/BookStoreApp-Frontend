f8239fd73a06daacb6e1923fa8e58633f4a14e93
Disha <dishakamble24@gmail.com>
2025-05-22 02:16:35 +0530

    [Disha] Added: for show loader

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { LoaderService } from '../../services/loader/loader.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent {
+  isLoading: Observable<boolean>;
+
+  constructor(private loaderService: LoaderService) {
+    this.isLoading = this.loaderService.loading$;
+  }
+}
